8e706fe914eb04fb676c8b5d37ce293a131de7a1
dasomko <ds92ko@gmail.com>
2025-01-07 16:30:19 +0900

    feat: WorkLog 컴포넌트 제거
    - 추후 출퇴근 시간 기록 시 컴포넌트 재사용 예정

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import Header from './lib/components/Header.svelte';
   import WorkSetup from './lib/components/WorkSetup.svelte';
   import WorkTracker from './lib/components/WorkTracker.svelte';
-  import WorkLog from './lib/components/WorkLog.svelte';
   import Toast from './lib/components/Toast.svelte';
   import Footer from './lib/components/Footer.svelte';
 
@@ -21,7 +20,6 @@
     <WorkSetup bind:isHalfDay bind:hasLunch bind:clockInTime />
     {:else}
     <WorkTracker {isHalfDay} {hasLunch} {clockInTime} {clockOutTime} />
-    <WorkLog {isHalfDay} {hasLunch} {clockInTime} {clockOutTime} />
     {/if}
   </div>
 </main>
