d6700b718a448d157e9005d51c26c80e0243bdbc
dasomko <ds92ko@gmail.com>
2025-01-07 11:59:05 +0900

    feat: Toast 컴포넌트 개발

---
+++ src/lib/components/Toast.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Snackbar } from 'attractions';
+
+  export let message;
+</script>
+
+{#if message}
+  <div class="toast">
+    <div class="container">
+      <Snackbar text={message} />
+    </div>
+  </div>
+{/if}
+
+<style>
+  .toast {
+    position: fixed;
+    bottom: 100px;
+    left: 0;
+    right: 0;
+    z-index: 500;
+  }
+</style>
