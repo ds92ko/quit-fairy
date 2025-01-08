85b64b317a443bece83c4240ace408a55d9c5237
dasomko <ds92ko@gmail.com>
2025-01-08 20:06:09 +0900

    fix: build 파일에 utils/format 추가

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ import { formatDate, formatTime, formatDuration, parseTime } from './src/lib/uti
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const loadURL = serve({ directory: 'public' });
 
 // Store 초기화
