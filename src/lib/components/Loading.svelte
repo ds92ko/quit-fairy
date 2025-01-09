6530816eaf4f1e4b29f6934b6599e0d2fabe686d
dasomko <ds92ko@gmail.com>
2025-01-09 18:33:32 +0900

    feat: loading 컴포넌트 추출

---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Loading } from 'attractions';
+</script>
+
+<div class="loading">
+  <Loading />
+  <p>잠시만 기다려주세요... ⏳✨</p>
+</div>
+
+<style>
+  .loading {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 20px;
+    padding-block: 50px;
+    text-align: center;
+  }
+</style>
