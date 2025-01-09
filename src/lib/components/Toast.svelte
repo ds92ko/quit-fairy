bfcc49ad8317e449955b0e2ae177c6b49b73223d
dasomko <ds92ko@gmail.com>
2025-01-09 18:36:43 +0900

    feat: toast 컴포넌트에서 notification 로직 제거

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,36 +1,14 @@
 <script>
   import { Snackbar } from 'attractions';
 
-  export let notification = { message: '', onlyToast: false };
-
-  const requestNotificationPermission = async () => {
-    if (Notification.permission !== 'granted') {
-      await Notification.requestPermission();
-    }
-  };
-
-  $: if (notification.message) {
-    if (!notification.onlyToast) {
-      requestNotificationPermission().then(() => {
-        new Notification('칼퇴 요정', { body: notification.message });
-      });
-    }
-    setTimeout(() => {
-      notification = {
-        message: '',
-        onlyToast: false,
-      };
-    }, 2000);
-  }
+  export let message = '';
 </script>
 
-{#if notification.message}
-  <div class="toast">
-    <div class="container">
-      <Snackbar text={notification.message} transitionOptions={{ x: 0, y: 20, duration: 150 }} closeCallback={() => {}} />
-    </div>
+<div class="toast">
+  <div class="container">
+    <Snackbar text={message} transitionOptions={{ x: 0, y: 20, duration: 150 }} closeCallback={() => {}} />
   </div>
-{/if}
+</div>
 
 <style>
   .toast {
